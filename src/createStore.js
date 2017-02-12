1a7fec6700ee721e3079cacfd790adcdf78a32c1
Rob Hitt <robhitt@gmail.com>
2017-02-11 22:32:38 -0500

    done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,38 @@
 export default function createStore(reducer){
+  let state;
 
+  function dispatch(action){
+    state = reducer(state,action)
+    render()
+  }
+
+  function getState(){
+    return state;
+  }
+
+  dispatch({type: '@@init'})
+
+  return {dispatch: dispatch, getState: getState}
 }
+
+
+
+  function render(){
+    let container = document.getElementById('container')
+    container.textContent = 'hi'
+  }
 
 // solution
-export default function createStore(reducer){
-}
+// export default function createStore(reducer){
+// }
 
-function render(){
-}
+// function candyReducer(state=[], action){
+//   switch (action.type) {
+//     case 'ADD_CANDY':
+//       return [...state, action.candy];
+//     default:
+//       return state;
+//   }
+// }
+//
+// export default candyReducer;
